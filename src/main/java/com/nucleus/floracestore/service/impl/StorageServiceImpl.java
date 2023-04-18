c299e55e7a8a359f9518af76d4fbae262d70d585
Kiril Kamenov <nucleus210@gmail.com>
2023-04-18 14:55:48 +0300

    Commit Changes

--- src/main/java/com/nucleus/floracestore/service/impl/StorageServiceImpl.java
+++ src/main/java/com/nucleus/floracestore/service/impl/StorageServiceImpl.java
@@ -79,48 +79,6 @@ public class StorageServiceImpl implements StorageService {
     @Override
     public StorageServiceModel storeFile(MultipartFile file) {
 
-        // create a string of uppercase and lowercase characters and numbers
-        String upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
-        String numbers = "0123456789";
-
-        // combine all strings
-        String alphaNumeric = upperAlphabet + lowerAlphabet + numbers;
-
-        // create random string builder
-        StringBuilder sb = new StringBuilder();
-
-        // create an object of Random class
-        Random random = new Random();
-
-        // specify length of random string
-        int length = 10;
-
-        for(int i = 0; i < length; i++) {
-
-            // generate random index number
-            int index = random.nextInt(alphaNumeric.length());
-
-            // get character specified by index
-            // from the string
-            char randomChar = alphaNumeric.charAt(index);
-
-            // append the character to string builder
-            sb.append(randomChar);
-        }
-
-        String randomString = sb.toString();
-
-
-
-
-//        File.createTempFile(String prefix,
-//                String suffix,
-//                File director);
-        // Normalize file name
-//        String fileName = randomString + "." + getFileExtension(file.getOriginalFilename());
-//        String fileName = randomString + "." + getFileExtension(file.getOriginalFilename());
-
         String fileName = new SimpleDateFormat("yyyy-MM-dd'_'hh-mm-ss-SSS")
                 .format(new Date()) + "-file." + getFileExtension(file.getOriginalFilename());
         try {
